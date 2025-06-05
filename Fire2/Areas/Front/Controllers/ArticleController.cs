4dada175cf3c3db62848283e745b7e650ebd9fa7
Zem860 <zemmyhammy@gmail.com>
2025-06-05 16:45:44 +0800

    update: front page expert detail is changable

--- Fire2/Areas/Front/Controllers/ArticleController.cs
+++ Fire2/Areas/Front/Controllers/ArticleController.cs
@@ -37,19 +37,17 @@ namespace Fire2.Areas.Front.Controllers
             return View();
         }
         public ActionResult ExpertDetail(int id)
-
         {
-            if (String.IsNullOrWhiteSpace(Convert.ToString(id)))
+            // 不需要 Convert.ToString(id)，因為 int 不能為 null，也不會是空字串
+            var expert = db.Experts.FirstOrDefault(e => e.Id == id);
+
+            if (expert == null)
             {
                 return HttpNotFound();
             }
-            var expert = db.Experts.FirstOrDefault(e => e.Id == id);
-            if (expert==null)
-                {
-                    return HttpNotFound();
-                }
-            
+
             return View(expert);
         }
+
     }
 }
