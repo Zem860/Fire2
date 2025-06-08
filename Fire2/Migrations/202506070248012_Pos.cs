151e6b369158451ce1d39ba64a254f4bf4e60215
Zem860 <zemmyhammy@gmail.com>
2025-06-08 16:07:36 +0800

    update: remove default value of ShowForLoggedInUsers of NavbarItems Model

---
+++ Fire2/Migrations/202506070248012_Pos.cs
@@ -0,0 +1,18 @@
+﻿namespace Fire2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Pos : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.NavbarItems", "Position", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.NavbarItems", "Position");
+        }
+    }
+}
---
+++ Fire2/Migrations/202506070308109_requiredLogin.cs
@@ -0,0 +1,18 @@
+﻿namespace Fire2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class requiredLogin : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.NavbarItems", "ShowForLoggedInUsers", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.NavbarItems", "ShowForLoggedInUsers");
+        }
+    }
+}
