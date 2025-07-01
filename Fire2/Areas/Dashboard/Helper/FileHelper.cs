a5fd7c9f98e901394bba3f9581126a29f8d26991
Zem860 <zemmyhammy@gmail.com>
2025-07-01 23:26:01 +0800

    feat: update FileHelper with new Path param

--- Fire2/Areas/Dashboard/Helper/FileHelper.cs
+++ Fire2/Areas/Dashboard/Helper/FileHelper.cs
@@ -8,11 +8,11 @@ namespace Fire2.Areas.Dashboard.Helper
 {
     public class FileHelper
     {
-        public static string SaveUpImage(HttpPostedFileBase upfile)
+        public static string SaveUpImage(HttpPostedFileBase upfile, string pos)
         {
             string extenstion = Path.GetExtension(upfile.FileName);
             string fileName = $"{DateTime.Now:yyyyMMddHHmmssfff}{extenstion}";
-            string path = HttpContext.Current.Server.MapPath("~/Uploads/Experts/");
+            string path = HttpContext.Current.Server.MapPath($"~/Uploads/{pos}/");
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
