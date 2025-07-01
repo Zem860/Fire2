51edccd5fda6506d3f577a00be6c5e5238624424
Zem860 <zemmyhammy@gmail.com>
2025-07-01 23:47:06 +0800

    feat:add FrontEnd News

---
+++ Fire2/Areas/Front/Controllers/NewsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fire2.Areas.Front.Models;
+using Fire2.Areas.Front.Helper;
+using Fire2.Models;
+using System.Net;
+
+
+namespace Fire2.Areas.Front.Controllers
+{
+    public class NewsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Front/News
+        public ActionResult Index()
+        {
+            var newsData = db.News.OrderBy(e => e.Id).ToList();
+            ViewBag.NewsData = newsData;
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            var newsData = db.News.OrderBy(e => e.Id).ToList();
+            ViewBag.NewsData = newsData;
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(news);
+        }
+    }
+
+}
---
+++ Fire2/Areas/Front/Models/ServiceHistoryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Fire2.Areas.Front.Models
+{
+    public class ServiceHistoryViewModel
+    {
+
+
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(100)]
+        public string ServiceUnit { get; set; }
+
+
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(50)]
+        [Display(Name = "職稱")]
+        public string JobTitle { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "年必填")]
+        public int? StartYear { get; set; }
+
+        [Range(1, 12, ErrorMessage = "月必填")]
+        public int? StartMonth { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "年必填")]
+        public int? EndYear { get; set; }
+
+        [Range(1, 12, ErrorMessage = "月必填")]
+        public int? EndMonth { get; set; }
+    }
+}
