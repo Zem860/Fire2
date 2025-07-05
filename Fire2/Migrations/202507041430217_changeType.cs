2b6db3da591995f8710ac858634c42dc8585b0da
Zem860 <zemmyhammy@gmail.com>
2025-07-05 23:01:43 +0800

    feat:LoginMigrations

---
+++ Fire2/Migrations/202507041430217_changeType.cs
@@ -0,0 +1,20 @@
+﻿namespace Fire2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changeType : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Members", "TotalYears", c => c.String(nullable: false));
+            AlterColumn("dbo.Members", "TotalMonths", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Members", "TotalMonths", c => c.Int(nullable: false));
+            AlterColumn("dbo.Members", "TotalYears", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ Fire2/Migrations/202507050237379_Add_adminTable.cs
@@ -0,0 +1,33 @@
+﻿namespace Fire2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_adminTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Account = c.String(nullable: false, maxLength: 50),
+                        PasswordHash = c.String(nullable: false, maxLength: 100),
+                        Salt = c.String(maxLength: 100),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        Gender = c.Int(nullable: false),
+                        Email = c.String(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                        UpdatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Admins");
+        }
+    }
+}
