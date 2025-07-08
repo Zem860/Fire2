d6625575ae5fe8ef1078c94592c68eb9e4e715f1
Zem860 <zemmyhammy@gmail.com>
2025-07-08 15:31:18 +0800

    feat: viewmodel for news search

---
+++ Fire2/Areas/Dashboard/Models/NewsSearchModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Fire2.Areas.Front.Models;
+using MvcPaging;
+
+namespace Fire2.Areas.Dashboard.Models
+{
+    public class NewsSearchModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public DateTime? CreatedAtStart { get; set; }
+        public DateTime? CreatedAtEnd { get; set; }
+
+        public IPagedList<Experts> Results { get; set; }
+    }
+}
