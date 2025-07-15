1a12bcd1d647c2635b2768022f87a5a71015b3b0
Zem860 <zemmyhammy@gmail.com>
2025-07-15 16:21:52 +0800

    update: forget to add News Model and details

---
+++ Fire2/Areas/Front/Models/News.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+namespace Fire2.Areas.Front.Models
+{
+    public class News
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "編號")]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(100)]
+        [Display(Name = "標題")]
+        
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(300)]
+        [Display(Name = "封面照片")]
+        public string CoverPhoto { get; set; }
+
+        [AllowHtml]
+        [Column(TypeName = "nvarchar(max)")]
+        [Display(Name = "內容")]
+        public string Content { get; set; }
+
+        [Column(TypeName = "DATETIME")]
+        [Display(Name = "建立時間")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Column(TypeName = "DATETIME")]
+        [Display(Name = "更新時間")]
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
