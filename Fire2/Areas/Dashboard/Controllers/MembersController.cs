0c8b32093e257ee7cf34cb8b715b845ce2aee65b
Zem860 <zemmyhammy@gmail.com>
2025-07-26 08:01:19 +0800

    feat: add MemberDashboard Index page

---
+++ Fire2/Areas/Dashboard/Controllers/MembersController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Fire2.Areas.Dashboard.Filter;
+using Fire2.Areas.Front.Models;
+using Fire2.Models;
+
+namespace Fire2.Areas.Dashboard.Controllers
+{
+    [PermissionFilter]
+
+    public class MembersController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Dashboard/Members
+        public ActionResult Index()
+        {
+            return View(db.Members.ToList());
+        }
+
+        // GET: Dashboard/Members/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Members members = db.Members.Find(id);
+            if (members == null)
+            {
+                return HttpNotFound();
+            }
+            return View(members);
+        }
+
+        // GET: Dashboard/Members/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Members/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Account,PasswordHash,Salt,Name,Gender,Birthday,MembershipType,Phone,Mobile,Address,Email,IsInternationalMember,InternationalCertificatePath,CurrentOrgnization,JobTitle,HighestEducation,IsVerified,TotalYears,TotalMonths,CreatedAt,UpdatedAt")] Members members)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Members.Add(members);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(members);
+        }
+
+        // GET: Dashboard/Members/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Members members = db.Members.Find(id);
+            if (members == null)
+            {
+                return HttpNotFound();
+            }
+            return View(members);
+        }
+
+        // POST: Dashboard/Members/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Account,PasswordHash,Salt,Name,Gender,Birthday,MembershipType,Phone,Mobile,Address,Email,IsInternationalMember,InternationalCertificatePath,CurrentOrgnization,JobTitle,HighestEducation,IsVerified,TotalYears,TotalMonths,CreatedAt,UpdatedAt")] Members members)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(members).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(members);
+        }
+
+        // GET: Dashboard/Members/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Members members = db.Members.Find(id);
+            if (members == null)
+            {
+                return HttpNotFound();
+            }
+            return View(members);
+        }
+
+        // POST: Dashboard/Members/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Members members = db.Members.Find(id);
+            db.Members.Remove(members);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
